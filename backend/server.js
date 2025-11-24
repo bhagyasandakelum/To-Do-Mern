74a6384774849134756ed2f39e31661bd4133854
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2025-11-25 01:54:36 +0530

    bugs fixed backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import todoRoutes from './routes/todo.route.js';
 import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
