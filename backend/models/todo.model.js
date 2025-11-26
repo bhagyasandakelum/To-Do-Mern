1756189e437ebc2e853b7b9e1b6b16b1739fb47c
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2025-11-27 00:42:57 +0530

    completed

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -1,17 +1,19 @@
+// models/todo.model.js
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema(
+  {
     text: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     completed: {
-        type: Boolean,
-        default: false,
-    }
-},{timestamps:true})
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const Todo = mongoose.model("Todo", todoSchema);
-
-export default Todo;   
-
+export default mongoose.model("Todo", todoSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// CONNECT TO DATABASE BEFORE STARTING SERVER
+connectDB();
+
 app.use("/api/todos", todoRoutes);
-app.listen(5000, ()=>{
-    connectDB();
+
+app.listen(5000, () => {
     console.log("server started at http://localhost:5000");
 });
-
